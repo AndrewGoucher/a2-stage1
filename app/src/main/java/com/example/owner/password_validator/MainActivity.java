6e2fc2fde6c1c6f9e885e8755c9fc9fb8cdcad8f
AndrewGoucher <andrew_goucher@hotmail.ca>
2017-05-29 12:37:06 -0300

    Add files via upload

--- app/src/main/java/com/example/owner/password_validator/MainActivity.java
+++ app/src/main/java/com/example/owner/password_validator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView strengthOut;
     TextView passIn;
+
     Button check;
 
     @Override
@@ -25,10 +26,34 @@ public class MainActivity extends AppCompatActivity {
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String password=passIn.getText().toString();
-                if(password.length()<8){
-                    strengthOut.setText("length fail");
+                String password = passIn.getText().toString();
+                if (password.length() < 8) {
+                    String rule1 = "length fail";
+                    strengthOut.setText(rule1);
+                } else if (password.equals("password")) {
+                    String rule2 = "'password' is extremely weak, fail";
+                    strengthOut.setText(rule2);
+                } else if (password.equals("12345678")) {
+                    String rule3 = "'12345678' is emtremely weak, fail";
+                    strengthOut.setText(rule3);
+                } else if (password.contains("@")||password.contains("!")||password.contains("#")||password.contains("$")||password.contains("%")||password.contains("&")||password.contains("*")||password.contains("^")) {
+                    if(password.length()>12){
+                        String rule4 = "Strong Password, all rules passed";
+                        strengthOut.setText(rule4);
+                    }
+                    else {
+                        String rule5 = "moderate strength";
+                        strengthOut.setText(rule5);
+                    }
                 }
+                    else if (password.equals(password.length()>12)) {
+                        String rule6="intermediate password, special character fail";
+                        strengthOut.setText(rule6);
+                }
+                else{
+                    String rule7= "weak password, entended length and special character fail";
+                }
+                // rule 1: password not allowed, rule 2: length of 8+ required, rule 3: 12345678 not allowed, rule 4: special character required, rule 5: extended length of 12+ required for next tier password strength
             }
         });
     }
